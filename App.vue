9ffd29cac4f301cbc8923dad5e7c19ba3440090e
hjjluotuo <hjjluotuo@163.com>
2025-03-30 14:46:17 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Gantt Scheduler</h1>
+    <GanttChart :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import GanttChart from './components/GanttChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GanttChart,
+  },
+  data() {
+    return {
+      tasks: [
+        { name: 'Task 1', start: '2023-10-01', end: '2023-10-05' },
+        { name: 'Task 2', start: '2023-10-05', end: '2023-10-10' },
+        { name: 'Task 3', start: '2023-10-10', end: '2023-10-15' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
